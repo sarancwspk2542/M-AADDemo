f8053fb1c8f759384a290cc6ae8d043782b3f11d
sarancwspk2542 <sarancwspk2542@hotmail.com>
2021-12-10 12:45:12 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AADDemo.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.IO;
+using Grpc.Core;
+using AADDemo.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace AADDemo.Controllers
+{
+
+    [Authorize(Roles = ("User,Admin"))]
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment Environment;
+        public HomeController(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+        public IActionResult Index()
+        {
+            string[] filepaths = Directory.GetFiles(Path.Combine(this.Environment.WebRootPath, "Files/"));
+
+            List<FileModel> list = new List<FileModel>();
+            foreach (string filepath in filepaths)
+            {
+                list.Add(new FileModel { FileName = Path.GetFileName(filepath) });
+            }
+            return View(list);
+        }
+
+        public FileResult DownloadFile(string filename)
+        {
+            string path = Path.Combine(this.Environment.WebRootPath, "Files/") + filename;
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            return File(bytes, "application/octet-stream", filename);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            //ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        ///////////////////////////////
+
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Privacy(IFormFile file)
+        {
+            await UploadFile(file);
+            TempData["msg"] = "File Uploaded successfully.";
+            return View();
+        }
+        // Upload file on server
+        public async Task<bool> UploadFile(IFormFile file)
+        {
+            string path = "";
+            bool iscopied = false;
+            try
+            {
+                if (file.Length > 0)
+                { 
+                    string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    path = Path.GetFullPath(Path.Combine(this.Environment.WebRootPath, "Files/"));
+                    using (var filestream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(filestream);
+                    }
+                    iscopied = true;
+                }
+                else
+                {
+                    iscopied = false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return iscopied;
+        }
+
+        public IActionResult Delete(string imgdel)
+        {
+            imgdel = Path.Combine(this.Environment.WebRootPath, "Files/", imgdel);
+            FileInfo fi = new FileInfo(imgdel);
+            if(fi != null)
+            {
+                System.IO.File.Delete(imgdel);
+                fi.Delete();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Models/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADDemo.Models
+{
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public string FileName { get; set; }
+    }
+}
